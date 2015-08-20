f9981b9ac5aa53663377b93022b8e7464ebd4539
doubletong <service@heiniaozhi.cn>
2015-08-20 20:01:51 +0800

    Update

---
+++ SIGOA/SIG.WebUI/Models/DataContextInitilizer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIG.WebUI.Models
+{
+
+    public class DataContextInitilizer : DropCreateDatabaseIfModelChanges<DataContext>
+    {
+
+        protected override void Seed(DataContext context)
+        {
+            InitializeIdentityForEF(context);
+            base.Seed(context);
+        }
+
+
+        //Create User=Admin@Admin.com with password=Admin@123456 in the Admin role        
+        public static void InitializeIdentityForEF(DataContext db)
+        {
+            Role role1 = new Role { RoleName = "Admin" };
+            Role role2 = new Role { RoleName = "User" };
+
+            User user1 = new User { Username = "admin", Email = "admin@ymail.com", FirstName = "Admin", Password = "123456", IsActive = true, CreateDate = DateTime.UtcNow, Roles = new List<Role>() };
+            User user2 = new User { Username = "user1", Email = "user1@ymail.com", FirstName = "User1", Password = "123456", IsActive = true, CreateDate = DateTime.UtcNow, Roles = new List<Role>() };
+            user1.Roles.Add(role1);
+            user2.Roles.Add(role2);
+            db.Users.Add(user1);
+            db.Users.Add(user2);
+
+            db.SaveChanges();
+        }
+    }
+}
